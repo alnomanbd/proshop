2ea54270d3f6a6e8e5d580844cdef5c35560d369
CySecLab <CySecLab@L-DFS-FT0541.upaybd.com>
2024-04-16 17:20:13 +0600

    frontend with reduct

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import {Provider} from 'react-redux'
+import store from './store.js'
+
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
 import App from './App.jsx'
-import './index.css'
+//import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/styles/bootstrap.custom.css'
+import './assets/styles/index.css'
+import HomeScreen from './screens/HomeScreen.jsx'
+import ProductScreen from './screens/ProductScreen.jsx'
+
+import axios from 'axios'
+axios.defaults.baseURL = `http://localhost:5000`
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+      <Route index={true} path='/' element={<HomeScreen/>}/>
+      <Route path='/product/:id' element={<ProductScreen/>}/>
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router}/>
+    </Provider>
   </React.StrictMode>,
 )
